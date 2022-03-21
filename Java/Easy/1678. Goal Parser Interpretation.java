a8a0e55bdfc401d24a062be006f7d98caa5e39bb
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2022-03-21 16:50:59 +0530

    Create 1678. Goal Parser Interpretation.java

---
+++ Java/Easy/1678. Goal Parser Interpretation.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String interpret(String command) {
+        
+      command = command.replace("()", "o");
+         command = command.replace("(al)", "al");
+         return  command;
+        
+    }
+}
