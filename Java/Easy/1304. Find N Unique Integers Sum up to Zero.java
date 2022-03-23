f254936f66537bef466c1a7b83af62e82d535fff
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2022-03-23 12:30:32 +0530

    Update 1304. Find N Unique Integers Sum up to Zero.java

--- Java/Easy/1304. Find N Unique Integers Sum up to Zero.java
+++ Java/Easy/1304. Find N Unique Integers Sum up to Zero.java
@@ -6,3 +6,18 @@ class Solution {
         return A;
     }
 }
+
+// Approach 2
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] result = new int[n];
+        int l = 0, r = result.length - 1;
+        while (l < r) {
+            result[l] = r;
+            result[r] = -r;
+            l++; r--;
+        }
+        return result;
+    }
+}
