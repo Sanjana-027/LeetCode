f06163cc5fbd401288012b35e635c35893109f4e
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2022-04-07 11:34:30 +0530

    Update 90. Subsets II.java

--- Java/Medium/90. Subsets II.java
+++ Java/Medium/90. Subsets II.java
@@ -1,3 +1,5 @@
+// 1st Approach
+
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
          List<List<Integer>> list = new ArrayList<>();
@@ -10,6 +12,27 @@ class Solution {
 
         for(int i = start; i < nums.length; i++){
           if (i > start && nums[i] == nums[i-1]) continue;
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
+
+// 2nd Approach
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+         List<List<Integer>> list = new ArrayList<>();
+         Arrays.sort(nums);
+         backtrack(list, new ArrayList<>(), nums, 0);
+         return list;
+    }
+    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        if (!list.contains(tempList))// check
+        list.add(new ArrayList<>(tempList));
+
+        for(int i = start; i < nums.length; i++){
             tempList.add(nums[i]);
             backtrack(list, tempList, nums, i + 1);
             tempList.remove(tempList.size() - 1);
