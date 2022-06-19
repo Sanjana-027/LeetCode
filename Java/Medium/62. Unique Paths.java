2d0ad9a2dee9cc33b3d3da4bb8d359c327a04c40
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2022-06-19 22:09:15 +0530

    Update 62. Unique Paths.java

--- Java/Medium/62. Unique Paths.java
+++ Java/Medium/62. Unique Paths.java
@@ -16,3 +16,24 @@ class Solution {
 		return memo.get(key);
     }
 }
+
+// BU USING 2D ARRAY ---> [EFFICIENT SOLUTION]
+
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        return uniquePathsHelper(m, n, new int[m + 1][n + 1]);
+    }
+
+    private int uniquePathsHelper(int m, int n, int[][] memo) {
+        if (m == 1 || n == 1) 
+            return 1;
+        
+        if (memo[m][n] != 0) 
+            return memo[m][n];
+        
+        memo[m][n] = uniquePathsHelper(m - 1, n, memo) + uniquePathsHelper(m, n - 1, memo);
+        return memo[m][n];
+        
+    }
+}
