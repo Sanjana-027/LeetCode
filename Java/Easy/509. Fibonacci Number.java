c9d126d747bb0777d042881fce935e9e1aef422d
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2022-06-20 19:23:54 +0530

    Update 509. Fibonacci Number.java

--- Java/Easy/509. Fibonacci Number.java
+++ Java/Easy/509. Fibonacci Number.java
@@ -24,3 +24,20 @@ class Solution {
         return num1;
     }
 }
+
+//3rd Approach DP
+class Solution {
+    public int fib(int n) {
+        int[] memo = new int[n + 1]; 
+        return dp(n, memo);
+    }
+    private int dp(int n, int[] memo) {
+        if(memo[n] != 0){
+            return memo[n];
+        }
+        if(n == 1 || n == 0) return n;
+        
+        memo[n] =  dp(n - 1, memo) + dp(n - 2, memo);
+        return memo[n];
+    }
+}
