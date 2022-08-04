aa2610c6fb69bc58fff48ae9f6c8b9cef5c52901
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2022-08-04 16:14:05 +0530

    Update 1. Two Sum.java

--- Java/Easy/1. Two Sum.java
+++ Java/Easy/1. Two Sum.java
@@ -17,3 +17,27 @@ class Solution {
         return answer;
     }
 }
+
+// 2nd Approach
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if(hm.containsKey(diff))
+                return new int[]{hm.get(diff), i};
+            
+            hm.put(nums[i], i);
+        }
+         return new int[]{0, 0};
+    }
+}
+
+    
+// 17 - 2 = 15 -----> 2 -> 0
+// 17 - 7 = 10 -----> 7 -> 1
+// 17 - 11 = 6 -----> 11 -> 2
+// 17 - 15 = 2
+
+// ans - x = y
