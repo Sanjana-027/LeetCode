2e27a73f96c5710ee6ba9b9bf2a0e6ad51d1be68
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2022-08-04 17:55:39 +0530

    Create 121. Best Time to Buy and Sell Stock.java

---
+++ Java/Easy/121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice)
+                minPrice = prices[i];
+            else if (prices[i] - minPrice > maxProfit)
+                maxProfit = prices[i] - minPrice;
+        }
+        return maxProfit;
+    }
+} 
