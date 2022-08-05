49dbb184627c4d9e324799d10963fc2b60de58eb
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2022-08-05 12:58:48 +0530

    Update 680. Valid Palindrome II.java

--- Java/Easy/680. Valid Palindrome II.java
+++ Java/Easy/680. Valid Palindrome II.java
@@ -22,3 +22,24 @@ lass Solution {
         return true;
     }
 }
+
+// 2nd Approach
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        return isPalindrome(s, 0, s.length() - 1, true);
+    }
+    
+    private boolean isPalindrome(String s, int start, int end, boolean flag) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                if(flag)
+                    return isPalindrome(s, start + 1, end, false) || isPalindrome(s, start, end - 1, false);
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
