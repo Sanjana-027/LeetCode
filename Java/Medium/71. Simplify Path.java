e7349e6150ab2a31c2b9403c66f769c97113f227
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2022-09-06 11:55:53 +0530

    Create 71. Simplify Path.java

---
+++ Java/Medium/71. Simplify Path.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<String>();
+        StringBuilder res = new StringBuilder();
+        String[] p = path.split("/");
+        
+        for (int i = 0; i < p.length; i++){
+            if (!stack.isEmpty()  && p[i].equals("..")) 
+                stack.pop();
+            else if (!p[i].equals("") && !p[i].equals(".") && !p[i].equals(".."))
+                stack.push(p[i]);
+        }
+        if(stack.isEmpty()) return "/";
+        while(!stack.isEmpty()) {
+            res.insert(0,stack.pop()).insert(0,"/");
+        }
+        return res.toString();
+    }
+} 
+// ["home", "", "foo", "abcd", "..", "efg"]
+
+// /home/foo/ efg
+// haan     naaa
