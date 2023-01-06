828ec201e8ee04221ed80304a1505ab43567462d
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2023-01-06 19:37:09 +0530

    Create 1833. Maximum Ice Cream Bars.java

---
+++ Java/Medium/1833. Maximum Ice Cream Bars.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int sum = 0, count = 0;
+        for (int i = 0; i < costs.length; i++) {
+            sum += costs[i];
+            if (sum <= coins) {
+                count++;
+            }
+            else {
+                sum -= costs[i];
+            }
+        }
+        return count;
+    }
+}
