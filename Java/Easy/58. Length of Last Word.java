9495a9cbc24291b43c17133aaefd75e43fcbc0f2
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2023-05-09 22:33:17 +0530

    Update 58. Length of Last Word.java

--- Java/Easy/58. Length of Last Word.java
+++ Java/Easy/58. Length of Last Word.java
@@ -3,3 +3,16 @@ class Solution {
         return (s.trim().length() - 1) - (s.trim().lastIndexOf(" "));
     }
 }
+
+// 2nd Approach
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s.trim();
+        String[] arr = s.split(" ");
+
+        return arr[arr.length - 1].length();
+
+    }
+}
+
