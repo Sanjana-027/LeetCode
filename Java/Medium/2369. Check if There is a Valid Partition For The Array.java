2340a2772b82a1419ffeedaf689a3f9045190663
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2023-08-13 22:30:37 +0530

    Create 2369. Check if There is a Valid Partition For The Array.java

---
+++ Java/Medium/2369. Check if There is a Valid Partition For The Array.java
@@ -0,0 +1,28 @@
+class Solution {
+    Map<Integer, Boolean> memo = new HashMap<>();
+
+    boolean prefixIsValid(int[] nums, int i) {
+        if (memo.containsKey(i)) {
+            return memo.get(i);
+        }
+        boolean ans = false;
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            ans |= prefixIsValid(nums, i - 2);
+        }
+        if (i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]) {
+            ans |= prefixIsValid(nums, i - 3);
+        }
+        if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i - 1] == nums[i - 2] + 1) {
+            ans |= prefixIsValid(nums, i - 3);
+        }
+        memo.put(i, ans);
+        return ans;
+    }
+
+    public boolean validPartition(int[] nums) {
+        int n = nums.length;
+        memo.put(-1, true);
+
+        return prefixIsValid(nums, n - 1);
+    }
+}
